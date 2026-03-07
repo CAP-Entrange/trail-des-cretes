a82dbbcf9c43a0ca337c6d7889646d19f492d448
Nicolas M <mine.nicolas@gmail.com>
2026-03-07 18:11:21 +0100

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/trail-des-cretes/" : "/",
   server: {
     host: "::",
     port: 8080,
